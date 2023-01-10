1b1db0187adb396fbbae0a586dbd09ec29469bb4
Soroosh Noorzad <soroosh.noorzad@gmail.com>
2023-01-10 20:15:47 +0330

    Update P2P_Con.py

--- P2P_Con.py
+++ P2P_Con.py
@@ -1,4 +1,12 @@
+# If not installed, install this lib first:
+# pip install pdf2image
+
 from pdf2image import convert_from_path
-pages = convert_from_path('form.pdf', 500)
-for page in pages:
-    page.save('out.jpg', 'JPEG')
+
+INPUT_PDF_NAME = "input_pdf.pdf"
+DPI = 100
+TARGET_FORMAT = "JPEG"
+
+pages = convert_from_path(INPUT_PDF_NAME, DPI)
+for idx, page in enumerate(pages):
+    page.save("page_" + str(idx) + ".jpg", TARGET_FORMAT)
